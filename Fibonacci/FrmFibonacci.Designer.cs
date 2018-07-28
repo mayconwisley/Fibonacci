471fb64b64f615d29863e5d4fbb5d3881171c73e
Maycon Wisley <Maycon Wisley@DESKTOP-C9VK6KA>
2018-07-28 18:08:40 -0300

    Fibonacci

--- Fibonacci/FrmFibonacci.Designer.cs
+++ Fibonacci/FrmFibonacci.Designer.cs
@@ -40,9 +40,9 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(12, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(101, 13);
+            this.label1.Size = new System.Drawing.Size(89, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Num. de Sequencia";
+            this.label1.Text = "Num.  Sequência";
             // 
             // TxtNumSequencia
             // 
